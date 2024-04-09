42b8b6d0946207f10dae711664570c6a5415f955
xzp474 <31110859+xzp474@users.noreply.github.com>
2024-04-09 15:21:34 +0800

    Delete lib directory

--- lib/src/components/components.dart
+++
@@ -1,4 +0,0 @@
-export 'dog.dart';
-export 'background.dart';
-export 'ground.dart';
-export 'follow_target.dart';
--- lib/src/config.dart
+++
@@ -1,5 +0,0 @@
-const gameWidth = 820.0;
-const gameHeight = 1600.0;
-const dogRadius = gameWidth * 0.05;
-const gameSpeed = 200.0;
-const backWidth = 2700;
